58fe27722bce0761fd03bb265e3a8e8d7b7bba21
assout <assout@users.noreply.github.com>
2016-08-27 23:56:20 +0900

    登録日時を追加

--- s3trigger/index.js
+++ s3trigger/index.js
@@ -20,6 +20,7 @@ function registrationMetadata(event, context) {
     TableName: 'images',
     Item: {
       bucket: `https://s3-ap-northeast-1.amazonaws.com/${bucket}/${key}`,
+      registeredTime: Date.now(),
       referenceTime: 0,
       size,
       eventName,
